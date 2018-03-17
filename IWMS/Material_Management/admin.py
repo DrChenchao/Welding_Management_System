2a9da0601fc9172f7b41fccf6e3d390adaf320e2
Immestrella <40486163@sjtu.edu.cn>
2018-03-17 18:51:08 +0800

    修改了下base.html中的一个错误，为materialManagement添加表格

--- IWMS/Material_Management/admin.py
+++ IWMS/Material_Management/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from Material_Management.models import WeldingMachine, WeldingMaterial, Auxiliary, BaseMetal, ProtectiveGas
 
-# Register your models here.
+admin.site.register(WeldingMaterial)
+
+admin.site.register(WeldingMachine)
+
+admin.site.register(Auxiliary)
+
+admin.site.register(BaseMetal)
+
+admin.site.register(ProtectiveGas)
--- IWMS/Material_Management/views.py
+++ IWMS/Material_Management/views.py
@@ -1,31 +1,31 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from Material_Management.models import *
 # Create your views here.
 
 @login_required
 def auxilary_material(request):
-    pass
-    return render(request, 'Material_Management/auxilary_material.html')
+    auxilarys = Auxiliary.objects.all()
+    return render(request, 'Material_Management/auxilary_material.html',locals())
 
 @login_required
 def base_metal(request):
-    pass
-    return render(request, 'Material_Management/base_metal.html')
+    baseMetals = BaseMetal.objects.all()
+    return render(request, 'Material_Management/base_metal.html',locals())
 
 @login_required
 def welding_gas(request):
-    pass
-    return render(request, 'Material_Management/welding_gas.html')
+    weldingGases = ProtectiveGas.objects.all()
+    return render(request, 'Material_Management/welding_gas.html',locals())
 
 @login_required
 def welding_material(request):
-    pass
-    return render(request, 'Material_Management/welding_material.html')
+    weldingMaterials = WeldingMaterial.objects.all()
+    return render(request, 'Material_Management/welding_material.html',locals())
 
 @login_required
 def welding_torch(request):
-    pass
-    return render(request, 'Material_Management/welding_torch.html')
+    weldingTorchs = WeldingMachine.objects.all()
+    return render(request, 'Material_Management/welding_torch.html',locals())
 
