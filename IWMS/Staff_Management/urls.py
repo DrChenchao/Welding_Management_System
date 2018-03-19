9f0b877b37008a4683f79accb2bbb9f125bbc420
DrChenchao <emailofCC@sjtu.edu.cn>
2018-03-19 05:26:02 -0700

    finish the department part

--- IWMS/Staff_Management/urls.py
+++ IWMS/Staff_Management/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'Staff_Management'
 urlpatterns = [
     url(r'^staff_management/$',views.staff_management,name='staff_management'),
+    url(r'^department_creat/$',views.department_creat,name='department_creat'),
+    url(r'^department_edit/$',views.department_edit,name='department_edit'),
+    url(r'^department_delete/$',views.department_delete,name='department_delete'),
 ]
--- IWMS/Staff_Management/views.py
+++ IWMS/Staff_Management/views.py
@@ -2,11 +2,38 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from Staff_Management.models import *
+from django.http import HttpResponse
 # Create your views here.
 
 @login_required
 def staff_management(request):
-    pass
-    return render(request, 'Staff_Management/staff_management.html')
+	departments = Department.objects.all()
+	return render(request, 'Staff_Management/staff_management.html',locals())
 
+@login_required
+def department_creat(request):
+	name = "";
+	if request.method == 'GET':
+		name = request.GET['name']
+		p = Department(name = name)
+		p.save()
+	return HttpResponse(request.GET['name'])
 
+@login_required
+def department_edit(request):
+	name = "";
+	old_content="";
+	if request.method == 'GET':
+		name = request.GET['name']
+		old_content = request.GET['old_content']
+		p = Department.objects.filter(name=old_content).update(name = name)
+	return HttpResponse(name)
+
+@login_required
+def department_delete(request):
+	old_content="";
+	if request.method == 'GET':
+		old_content = request.GET['old_content']
+		Department.objects.filter(name=old_content).delete();
+	return HttpResponse(None)
